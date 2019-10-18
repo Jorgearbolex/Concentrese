13f3992215ae90e2ad258744e00dda4711ff812d
Jorgearbolex <jorgearbolex@gmail.com>
2019-10-18 16:56:47 -0500

    Initial commit

---
+++ Assets/Scripts/ControlCubo.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlCubo : MonoBehaviour
+{
+  
+    public bool show = false;
+
+    public bool tohide = false;
+
+    void Awake()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    private void Update() 
+    {
+        /*if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.up * 180, 2 * Time.deltaTime));
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.zero, 3 * Time.deltaTime));
+        }*/
+    }
+
+    private void OnMouseDown() 
+    {
+        
+        if (General.contClicks<2)
+        {
+            General.contClicks++;
+            if (General.contClicks==1)
+            {
+                General.selecA=gameObject;
+            }
+            else if (General.contClicks==2)
+            {
+                General.selecB=gameObject;
+            }
+            
+            
+        }
+        
+        
+
+    }
+
+    /*public void Verification()
+    {
+        if (General.cubesVerificator[0].GetComponent<CardsControler>().mID == General.cubesVerificator[1].GetComponent<CardsControler>().mID)
+        {
+          
+            Invoke("CubeDestroy", 1.7f);
+        }
+        else
+        {
+     
+            Invoke("NoConcidence", 1.7f);
+        }
+    }
+
+    public void CubeDestroy()
+    {
+        Destroy(General.cubesVerificator[0].gameObject);
+        Destroy(General.cubesVerificator[1].gameObject);
+      
+        ++General.winConditional;
+    }
+
+    public void NoConcidence()
+    {
+        General.cubesVerificator[0].gameObject.GetComponent<CardsControler>().show = false;
+        General.cubesVerificator[1].gameObject.GetComponent<CardsControler>().show = false;
+        General.cubesVerificator[0] = null;
+        General.cubesVerificator[1] = null;
+        
+    }*/
+
+    /*private void Update() 
+    {
+        if (General.time <= 0)
+        {
+            
+            if (General.cubesVerificator[0] != null)
+                General.cubesVerificator[0].gameObject.GetComponent<CardsControler>().show = false;
+            if (General.cubesVerificator[1] != null)
+                General.cubesVerificator[1].gameObject.GetComponent<CardsControler>().show = false;
+            
+        }
+
+        if (show == true)
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.up * 180, 2 * Time.deltaTime));
+        else
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.zero, 2 * Time.deltaTime));
+    }*/
+}
+
---
+++ Assets/Scripts/General.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class General : MonoBehaviour
+{
+    public GameObject[] materialetes = new GameObject[8];
+    public static GameObject selecA, selecB;
+    public static int contClicks=0;
+    public static bool hide;
+    void Awake() 
+    {
+        materialetes=GameObject.FindGameObjectsWithTag("cubo");
+        foreach (var item in materialetes)
+        {
+            //item.Getcomponent
+        }
+     
+    }
+
+    void Start () 
+    {
+       
+        
+
+
+
+
+
+
+    }
+
+    void Update()
+    {
+        /*if (selecA!=null||selecB!=null)
+        {
+            selecA.transform.rotation = Quaternion.Euler(Vector3.Lerp(selecA.transform.rotation.eulerAngles, Vector3.up * 180, 2 * Time.deltaTime));
+            selecB.transform.rotation = Quaternion.Euler(Vector3.Lerp(selecB.transform.rotation.eulerAngles, Vector3.up * 180, 2 * Time.deltaTime));
+        }*/
+
+
+        if (contClicks==1)
+        {
+            selecA.transform.rotation = Quaternion.Euler(Vector3.Lerp(selecA.transform.rotation.eulerAngles, Vector3.up * 180, 2 * Time.deltaTime));
+        }
+        else if (contClicks==2)
+        {
+            selecB.transform.rotation = Quaternion.Euler(Vector3.Lerp(selecB.transform.rotation.eulerAngles, Vector3.up * 180, 2 * Time.deltaTime));
+            Invoke("ActiveHide", 3);
+            
+        }
+
+        if (hide)
+        {
+            selecA.transform.rotation = Quaternion.Euler(Vector3.Lerp(selecA.transform.rotation.eulerAngles, Vector3.zero, 3 * Time.deltaTime));
+            selecB.transform.rotation = Quaternion.Euler(Vector3.Lerp(selecB.transform.rotation.eulerAngles, Vector3.zero, 3 * Time.deltaTime));
+            if (contClicks==0)
+            {
+                hide=false;
+            }
+        }
+    
+    }
+
+    void ActiveHide()
+    {
+        hide=true;
+        contClicks=0;
+    }
+
+    /*void Hide()
+    {
+        selecA.transform.rotation = Quaternion.Euler(Vector3.Lerp(selecA.transform.rotation.eulerAngles, Vector3.zero, 3 * Time.deltaTime));
+        selecB.transform.rotation = Quaternion.Euler(Vector3.Lerp(selecB.transform.rotation.eulerAngles, Vector3.zero, 3 * Time.deltaTime));
+        hide=false;
+    }*/
+}
+
